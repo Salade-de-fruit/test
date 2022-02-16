6fed8eda41217a7ca9c8226761866ef7f7fb3f94
Guillaume BOYER <guillaume.boyer@esme.fr>
2022-02-16 10:07:30 +0100

    la troisième commission

--- src/main.java
+++ src/main.java
@@ -5,7 +5,8 @@ public class main {
 
     public static void main(String[] args )
     {
-                System.out.println("Le code explose BOUM BIM BAM BOUM BOUM BAM BIM PAF PIF POUF PAF PAF POUF BOUM BIM PAF");
+
+                System.out.println("Le code n'explose  pas BOUM BIM BAM BOUM BOUM BAM BIM PAF PIF POUF PAF PAF POUF BOUM BIM PAF");
 
 
     }
