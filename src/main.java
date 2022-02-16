7ba114cc46f67a00b3d58981d73b23b4ee4c6f2a
Guillaume BOYER <guillaume.boyer@esme.fr>
2022-02-16 09:38:50 +0100

    La première commission

---
+++ src/main.java
@@ -0,0 +1,17 @@
+public class main {
+
+
+    public static void main(String[] args )
+    {
+
+        System.out.println("L’affaire des fiches, parfois appelée l'affaire des casserolesN 1, est un scandale politique qui éclate en 1904 en France, sous la Troisième République. Il concerne une opération de fichage politique et religieux mise en place dans l'Armée française à l'initiative du général Louis André, ministre de la Guerre, dans un contexte de liquidation de l'affaire Dreyfus et d'accusations d'anti-républicanisme portées par la gauche à l'encontre du corps des officiers.\n" +
+                "\n" +
+                "De 1900 à 1904, l'administration préfectorale, les loges maçonniques du Grand Orient de France et d'autres réseaux de renseignement établissent des fiches sur les officiers, qui sont transmises au cabinet du général André afin de décider de l'avancement hiérarchique et des décorations à attribuer. Ces documents secrets sont préférés par André aux notations officielles du commandement militaire ; ils lui permettent de mettre en place un système où l'avancement des officiers républicains, francs-maçons ou libre-penseurs est favorisé tandis que la carrière des militaires nationalistes et catholiques — conviction religieuse qui vaut, pour le Grand Orient et le cabinet d'André, hostilité à la République — est entravée, dans le but de s'assurer de la loyauté de l'armée au régime en place.\n" +
+                "\n" +
+                "Le 28 octobre 1904, le député Jean Guyot de Villeneuve interpelle le gouvernement à la Chambre des députés et révèle le système de fichage instauré par le général André et le Grand Orient, produisant à l'appui de ses accusations des fiches qui lui ont été remises par Jean-Baptiste Bidegain, adjoint du secrétaire-général du Grand Orient. Le ministre nie avoir connaissance de ces agissements, mais durant la séance du 4 novembre, Guyot de Villeneuve produit un document qui incrimine André directement ; la séance est houleuse et le député nationaliste Gabriel Syveton gifle le ministre de la Guerre, déclenchant une empoignade dans l'hémicycle.\n" +
+                "\n" +
+                "Le scandale est important. Les rebondissements et les révélations se succèdent pendant plusieurs mois, tandis que la presse publie régulièrement les fiches en question. Malgré le soutien de Jean Jaurès et du Bloc des gauches, le gouvernement Émile Combes chute le 15 janvier 1905, emporté par l'affaire. Le cabinet Rouvier, qui lui succède, condamne formellement le fichage, prononce des sanctions symboliques et mène une politique d'apaisement. Néanmoins, le système des fiches se poursuit après 1905, appuyé non plus sur le Grand Orient mais sur les renseignements préfectoraux et adossé à la pratique de pressions politiques. En 1913, le ministre de la Guerre Alexandre Millerand y met fin définitivement.");
+
+
+    }
+}
